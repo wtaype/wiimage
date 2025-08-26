9847875f27b8135932b36d7db7922a46b17e9977
wtaype <wilder.taype16@gmail.com>
2025-08-25 23:26:55 -0500

    Actualizacion Tag

--- main.js
+++ main.js
@@ -1,7 +1,39 @@
 import $ from 'jquery';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import { auth, db } from './firebase/init.js'; // Importa la configuración de Firebase desde tu archivo de inicialización
+import { getFirestore,
+  setDoc, getDoc, deleteDoc, onSnapshot, // Lo mas usados
+  doc, collection, getDocs, serverTimestamp, // Muy utiles 
+  query, where, orderBy, limit } from "firebase/firestore";  //Para Firestore
+import { wiTema, Capi, Mensaje, Notificacion, savels, getls, removels, accederRol } from './widev.js'; //Tools geniales 
+
+// $('body').click(function(){
+//   alert('Igual, Dios te bendiga hermano!');
+// }); 
+
+$('h1').click(async function(){
+  Mensaje('Escribiendo en base de datos, espere...');
+  try {
+    const wisave = doc(db, 'publico', 'wilder');
+    await setDoc(wisave, {
+      nombre: 'Wilder',
+      apellidos: 'Taype Lapa', 
+      mensaje: 'Hola guapo hermoso Wilder, Dios te protege!',
+      creacion: serverTimestamp()
+    });
+    Mensaje('✅ Documento guardado exitosamente!');
+  }catch(e){console.error(e)}
+}); // Esto es para save - guardar el documento
 
 
-$('body').click(function(){
-  alert('Igual, Dios te bendiga hermano!');
-}); 
+$('button').click(async function(){
+  Mensaje('Esperando un mensaje ');
+  try {
+    const wiget = doc(db, 'publico', 'wilder');
+    const busq = await getDoc(wiget);
+    if (busq.exists()){
+      const data = busq.data();
+      Mensaje(data.mensaje);
+    }
+  }catch(e){console.error(e)}
+}); // Esto es para get -consultar el documento
