745a3c42df06b72238b197ed3aeaceaa9afc92a0
wtaype <wilder.taype16@gmail.com>
2025-08-27 10:57:19 -0500

    Actualizacion Principal 5

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/wiimage/', // Esta es la dirección para la versión más nueva
-})
+  base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        smile: resolve(__dirname, 'smile.html'),
+        smiletop: resolve(__dirname, 'smiletop.html'),
+      },
+    },
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  publicDir: 'public',
+});
